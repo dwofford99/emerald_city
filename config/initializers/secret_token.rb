ab0a8d9a8ba91bce5d366bc52f2d1da6ae90102d
David Wofford <dwofford99@yahoo.com>
2013-11-09 10:06:19 -0500

    new program for emerald city class

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '524554445933714dd46ed4272641c2c2a387fa821c27f99574b3dbed51cb26dcee61b35a762aa3e038a5ce3187f763e5f63cde6fa170f503767d835c7cc74514'
